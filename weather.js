a2a28a773e625e94849e820c3bb01740e1ad68f8
somyagupta1606-netizen <somyagupta1606@gmail.com>
2025-12-14 10:27:26 +0530

    UI Part Done

---
+++ weather.js
@@ -0,0 +1,18 @@
+let searchBtn = document.querySelector("#search-btn")
+let result = document.querySelector("#result");
+
+const getWeatherData = async ()=>{
+
+    try {
+        let cityName = document.querySelector("#city-name").value.trim;
+        if(!cityName){
+            result.innerHTML=`<p class="text-red-700 text-center mt-4 font-semibold">Please Enter a City Name........</p>`;
+        }
+    } catch (error) {
+        console.log(error,"Error in fetching Weather Details ");
+        
+        
+    }
+};
+
+searchBtn.addEventListener("click",getWeatherData)
